bb0a3645478e5855031b528cc6a0916dce04d4b6
Harsh Dobariya <harshdobariya101@gmail.com>
2022-04-12 15:37:22 +0530

    update folder structure

--- index.js
+++ index.js
@@ -6,90 +6,83 @@ fs.mkdir("config", { recursive: true }, (err) => {
     if (err) console.log(err);
 });
 
+fs.writeFile(
+    "config/database.js",
+    `const { info, mongooseDebug } = require("../debugging/debug"),
+mongoose = require("mongoose"),
+{ PORT, MONGODB_URL } = process.env;
+
+module.exports = (app) => {
+mongoose
+    .connect(MONGODB_URL)
+    .then(() => {
+        mongooseDebug(\`Server connected to MongoDB...\`);
+
+        app.listen(PORT, () => info(\`Server is running on...http://localhost:\${PORT}\`));
+    })
+    .catch(() => mongooseDebug(\`Server is not connected to MongoDB...\`));
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.writeFile(
+    "config/passport.js",
+    `const { Strategy, ExtractJwt } = require("passport-jwt"),
+    { User } = require("../models/user"),
+    { Seller } = require("../models/seller"),
+    { Admin } = require("../models/admin"),
+    passport = require("passport"),
+    options = {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_PRIVATE_KEY
+    };
+
+passport.use(
+    "user",
+    new Strategy(options, (decoded, done) => {
+        User.findById(decoded._id)
+            .then((user) => {
+                if (!user) return done(null, false);
+                done(null, user);
+            })
+            .catch((err) => done(err));
+    })
+);
+
+passport.use(
+    "seller",
+    new Strategy(options, (decoded, done) => {
+        Seller.findById(decoded._id)
+            .then((seller) => {
+                if (!seller) return done(null, false);
+                done(null, seller);
+            })
+            .catch((err) => done(err));
+    })
+);
+
+passport.use(
+    "admin",
+    new Strategy(options, (decoded, done) => {
+        Admin.findById(decoded._id)
+            .then((admin) => {
+                if (!admin) return done(null, false);
+                done(null, admin);
+            })
+            .catch((err) => done(err));
+    })
+);
+`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
 fs.mkdir("controllers", { recursive: true }, (err) => {
     if (err) console.log(err);
 });
-
-fs.mkdir("middleware", { recursive: true }, (err) => {
-    if (err) console.log(err);
-});
-
-fs.mkdir("debugging", { recursive: true }, (err) => {
-    if (err) console.log(err);
-});
-
-fs.mkdir("models", { recursive: true }, (err) => {
-    if (err) console.log(err);
-});
-
-fs.mkdir("routes", { recursive: true }, (err) => {
-    if (err) console.log(err);
-});
-
-fs.mkdir("start", { recursive: true }, (err) => {
-    if (err) console.log(err);
-});
-
-fs.writeFile(
-    "server.js",
-    `const app = require("express")();
-
-require("express-async-errors");
-require("./config/database")(app);
-require("./start/routes");`,
-    (err) => {
-        if (err) console.log(err);
-    }
-);
-
-fs.writeFile(
-    "config/mongoose.js",
-    `const { info, mongooseDebug } = require("../debugging/debug"),
-    mongoose = require("mongoose"),
-    { port, mongodb_url } = process.env;
-
-module.exports = (app) => {
-    mongoose
-        .connect(mongodb_url)
-        .then(() => {
-            mongooseDebug(\`Server connected to MongoDB...\`);
-
-            app.listen(port, () => info(\`Server is running on...http://localhost:\${port}\`));
-        })
-        .catch(() => mongooseDebug(\`Server is not connected to MongoDB...\`));
-};`,
-    (err) => {
-        if (err) console.log(err);
-    }
-);
-
-fs.writeFile(
-    "middleware/errors.js",
-    `const { error } = require("../debugging/debug");
-
-module.exports = (err, req, res, next) => {
-    error(err.name, err.code, err.status);
-
-    res.status(err.status || 500).send({error: err.message})
-};`,
-    (err) => {
-        if (err) console.log(err);
-    }
-);
-
-fs.writeFile(
-    "debugging/debug.js",
-    `const debug = require("debug");
-
-const info = debug("info"),
-    error = debug("error"),
-    mongooseDebug = debug("mongoose");
-
-module.exports = { info, error, mongooseDebug };`,
-    (err) => {
-        if (err) console.log(err);
-    }
-);
 
 fs.writeFile(
     "controllers/users.js",
@@ -100,6 +93,109 @@ module.exports = { };`,
         if (err) console.log(err);
     }
 );
+
+fs.mkdir("debugging", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+fs.writeFile(
+    "debugging/debug.js",
+    `const debug = require("debug");
+
+const info = debug("info"),
+    errors = debug("error"),
+    mongooseDebug = debug("mongoose"),
+
+module.exports = { info, errors, mongooseDebug};
+    `,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.mkdir("docs", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+fs.writeFile(
+    "docs/basicInfo.js",
+    `module.exports = {
+    openapi: "3.0.0",
+    info: {
+        version: "1.0.0",
+        title: "E-commerce API",
+        description: "A basic E-commerce API",
+        contact: {
+            name: "Harsh Dobariya",
+            email: "hdobariya229@gmail.com",
+            url: "https://github.com/Harsh-Dobariya"
+        }
+    }
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.writeFile(
+    "docs/servers.js",
+    `module.exports = {
+    servers: [
+        {
+            url: process.env.BASE_URL,
+            description: "Local server"
+        }
+    ]
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.writeFile(
+    "docs/index.js",
+    `const basicInfo = require("./basicInfo"),
+    servers = require("./servers"),
+    components = require("./components"),
+    tags = require("./tags"),
+    paths = require("./paths");
+
+module.exports = {
+    ...basicInfo,
+    ...servers
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.mkdir("middleware", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+fs.writeFile(
+    "middleware/errors.js",
+    `const { errors } = require("../debugging/debug");
+
+module.exports = (err, req, res, next) => {
+    errors(err.name, err.code, err.status);
+
+    let error = {};
+
+    if (Object.keys(error).length === 0) {
+        error[err.name] = err.message;
+    }
+
+    res.status(err.status || 500).send({ error, status_code: err.status });
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.mkdir("models", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
 
 fs.writeFile(
     "models/user.js",
@@ -115,6 +211,10 @@ module.exports = { User }`,
     }
 );
 
+fs.mkdir("routes", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
 fs.writeFile(
     "routes/users.routes.js",
     `const router = require("express").Router();
@@ -126,12 +226,40 @@ module.exports = router;`,
 );
 
 fs.writeFile(
-    "start/routes.js",
-    `module.exports = (app) => {
-    app.use(require("express").json());
-    app.use('/users', require("../routes/users.routes"));
+    "routes/index.js",
+    `const express = require("express"),
+    swaggerUI = require("swagger-ui-express"),
+    docs = require("../docs"),
+
+module.exports = (app) => {
+    app.use(require("cors")());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(require("passport").initialize());
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
+    app.use("/users", require("./users.routes"));
     app.use(require("../middleware/errors"));
-}`,
+};`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.mkdir("services", { recursive: true }, (err) => {
+    if (err) console.log(err);
+});
+
+fs.writeFile(
+    ".env.development",
+    `# Debugging configuration
+DEBUG=info,error,mongoose
+
+# Server configuration
+BASE_URL=http://localhost:3000
+PORT=3000
+
+# Database configuration
+MONGODB_URL=mongodb://localhost/Harsh`,
     (err) => {
         if (err) console.log(err);
     }
@@ -142,9 +270,47 @@ fs.writeFile(".gitignore", `node_modules/\n*.env.*`, (err) => {
 });
 
 fs.writeFile(
-    ".env.development",
-    `PORT=3000
-MONGODB_URL=mongodb://localhost/express-demo`,
+    "package.json",
+    `{
+    "name": "express-project",
+    "version": "1.0.0",
+    "description": "For practice",
+    "main": "server.js",
+    "author": "Harsh Dobariya",
+    "license": "MIT",
+    "scripts": {
+        "start": "node server.js",
+        "dev": "dotenv -e .env.development nodemon server.js"
+    },
+    "dependencies": {
+        "cors": "^2.8.5",
+        "express": "^4.17.3",
+        "express-async-errors": "^3.1.1",
+        "http-errors": "^2.0.0",
+        "mongoose": "^6.2.9",
+        "passport": "^0.5.2",
+        "passport-jwt": "^4.0.0",
+        "swagger-ui-express": "^4.3.0"
+    },
+    "devDependencies": {
+        "debug": "^4.3.4",
+        "dotenv-cli": "^5.1.0",
+        "nodemon": "^2.0.15"
+    }
+}`,
+    (err) => {
+        if (err) console.log(err);
+    }
+);
+
+fs.writeFile(
+    "server.js",
+    `const app = require("express")();
+
+require("express-async-errors");
+require("./config/mongoose")(app);
+require("./config/passport");
+require("./routes")(app);`,
     (err) => {
         if (err) console.log(err);
     }
